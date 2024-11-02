345d01bc3d7a98ad3c1427a534728dec51ebdad1
PeterMHammond <peter@everygoodwork.online>
2024-11-01 22:44:19 -0700

    What is the Gospel From:  https://gospel.ofgod.link/

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,10 @@ pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
             Response::ok("Jesus is Lord!")
         })
         .get_async("/about", |_, _| async move {
-            Response::ok("Welcome to our Christian website!")
+            Response::ok("Welcome to our Cloudflare Development Website!")
         })
-        .get_async("/verse", |_, _| async move {
-            Response::ok("For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life. - John 3:16")
+        .get_async("/gospel", |_, _| async move {
+            Response::ok("Gospel means good news! The bad news is we have all sinned and deserve the wrath to come. But Jesus the Messiah died for our sins, was buried, and then raised on the third day, according to the scriptures. He ascended into heaven and right now is seated at the Father's right hand. Jesus said, \"I am the way, and the truth, and the life. No one comes to the Father except through me. The time is fulfilled, and the kingdom of God is at hand; repent and believe in the gospel.\"")
         })
         .get_async("/healthcheck", |_, _| async move {
             Response::ok("OK")
